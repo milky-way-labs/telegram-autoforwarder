9abfa5824aa16734c2e63bcd42a27409861ddfb7
Alberto Giancarlo Ferrario <me@albertogferrar.io>
2025-01-12 01:48:36 +0100

    add genesis, disable bonkbot

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 import asyncio
-from telethon.sync import TelegramClient
+import os
+
 from telethon.errors import RpcCallFailError, PeerIdInvalidError
-from telethon.tl.types import PeerChannel, PeerChat, PeerUser
+from telethon.sync import TelegramClient
+from telethon.tl.types import PeerChannel
+import sqlite3
+
 
 class TelegramForwarder:
     def __init__(self, api_id, api_hash, phone_number):
@@ -10,6 +14,31 @@ class TelegramForwarder:
         self.phone_number = phone_number
         self.client_list_chats = TelegramClient('session_list_chats_' + phone_number, api_id, api_hash)
         self.client_forward_messages = TelegramClient('session_forward_messages_' + phone_number, api_id, api_hash)
+        # self.sql_connection = self.init_database()
+
+    @staticmethod
+    def init_database():
+        database_name = '.db'
+
+        if not os.path.exists(database_name):
+            print(f"Il database '{database_name}' non esiste. Creazione in corso...")
+
+            # Crea e inizializza il database
+            with sqlite3.connect(database_name) as conn:
+                cursor = conn.cursor()
+
+                # Esempio: creazione di una tabella di base
+                cursor.execute('''
+                CREATE TABLE IF NOT EXISTS bot (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    message TEXT NOT NULL,
+                    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+                )
+                ''')
+                conn.commit()
+                print(f"Il database '{database_name}' è stato creato con successo.")
+
+        return sqlite3.connect(database_name)
 
     async def list_chats(self):
         await self.client_list_chats.connect()
@@ -26,18 +55,20 @@ class TelegramForwarder:
 
         print("List of groups printed successfully!")
 
-    async def forward_messages_to_channel(self, source_chat_id, destination_chat_id, phrase, sniper_chat_id, quote_amount):
+    async def handle_genesis_gem_message(self, bonkbot_caller_chat_id):
         await self.client_forward_messages.connect()
 
         if not await self.client_forward_messages.is_user_authorized():
             await self.client_forward_messages.send_code_request(self.phone_number)
             await self.client_forward_messages.sign_in(self.phone_number, input('Enter the code: '))
 
+        genesis_gem_chat_id = -1002124901271  # Sostituisci con l'ID corretto o l'username del gruppo
+
         # Tenta di ottenere l'entità del gruppo sorgente
         try:
-            source_entity = await self.client_forward_messages.get_entity(PeerChannel(source_chat_id))
+            source_entity = await self.client_forward_messages.get_entity(PeerChannel(genesis_gem_chat_id))
         except ValueError as e:
-            print(f"Error retrieving entity for source chat ID {source_chat_id}: {e}")
+            print(f"Error retrieving entity for genesis gem: {e}")
             return
 
         # Ottiene l'ultimo messaggio
@@ -51,7 +82,67 @@ class TelegramForwarder:
         while True:
             print("Checking for messages and forwarding them...")
             try:
-                messages = await self.client_forward_messages.get_messages(source_entity, min_id=last_message_id, limit=None)
+                messages = await self.client_forward_messages.get_messages(source_entity, min_id=last_message_id,
+                                                                           limit=None)
+            except (RpcCallFailError, PeerIdInvalidError) as e:
+                print(f"Error retrieving messages: {e}")
+                break
+
+            for message in reversed(messages):
+                if message.id == last_message_id:
+                    break
+
+                hot_usernames = [
+                    'bitcoinbilliam'
+                    'Oppkun',
+                    'dave3xx',
+                    'Vsanek3',
+                    'tuanqu',
+                    'CryptMonkk',
+                    'Crypzypotela',
+                    'ultrastarlife'
+                ]
+
+                if message.from_user and message.from_user.username in hot_usernames:
+                    print(f"Message sent from: {message.from_user.username}")
+
+                    await self.client_forward_messages.send_message(bonkbot_caller_chat_id,
+                                                                    f"{message.from_user.username}\n\n{message.text}")
+                    print("Message forwarded")
+
+                last_message_id = max(last_message_id, message.id)
+
+            await asyncio.sleep(5)
+
+    async def handle_bonkbot_message(self, bonkbot_caller_chat_id):
+        await self.client_forward_messages.connect()
+
+        if not await self.client_forward_messages.is_user_authorized():
+            await self.client_forward_messages.send_code_request(self.phone_number)
+            await self.client_forward_messages.sign_in(self.phone_number, input('Enter the code: '))
+
+        bonkbot_chat_id = -1002086003521  # Sostituisci con l'ID corretto o l'username del gruppo
+
+        # Tenta di ottenere l'entità del gruppo sorgente
+        try:
+            source_entity = await self.client_forward_messages.get_entity(PeerChannel(bonkbot_chat_id))
+        except ValueError as e:
+            print(f"Error retrieving entity for bonkbot")
+            return
+
+        # Ottiene l'ultimo messaggio
+        try:
+            last_message_id = (await self.client_forward_messages.get_messages(source_entity, limit=1))[0].id
+            print(f"Last message ID: {last_message_id}")
+        except (RpcCallFailError, PeerIdInvalidError) as e:
+            print(f"Error retrieving messages: {e}")
+            return
+
+        while True:
+            print("Checking for messages and forwarding them...")
+            try:
+                messages = await self.client_forward_messages.get_messages(source_entity, min_id=last_message_id,
+                                                                           limit=None)
             except (RpcCallFailError, PeerIdInvalidError) as e:
                 print(f"Error retrieving messages: {e}")
                 break
@@ -62,19 +153,23 @@ class TelegramForwarder:
 
                 if message.text and '**🔥 BURNED LP 🔥**' in message.text and '**Open time:** `in' in message.text:
                     print(f"Message contains the phrase: {message.text}")
-                    
-                    await self.client_forward_messages.send_message(destination_chat_id, message.text)
+
+                    await self.client_forward_messages.send_message(bonkbot_caller_chat_id, message.text)
                     print("Message forwarded")
-                    
+
+                    defi_solana_sniper_chat_id = 6661880210  # sniper tg bot
+                    quote_amount = 0.0046
+
                     ca = message.text.split("**Mint:** `")[1].split("`")[0]
                     snipe_message = f"/snp {ca} {quote_amount}"
                     print(snipe_message)
-                    await self.client_forward_messages.send_message(sniper_chat_id, snipe_message)
+                    await self.client_forward_messages.send_message(defi_solana_sniper_chat_id, snipe_message)
                     print("Snipe set")
 
                 last_message_id = max(last_message_id, message.id)
 
             await asyncio.sleep(5)
+
 
 def read_credentials():
     try:
@@ -88,11 +183,13 @@ def read_credentials():
         print("Credentials file not found.")
         return None, None, None
 
+
 def write_credentials(api_id, api_hash, phone_number):
     with open("credentials.txt", "w") as file:
         file.write(api_id + "\n")
         file.write(api_hash + "\n")
         file.write(phone_number + "\n")
+
 
 async def main():
     api_id, api_hash, phone_number = read_credentials()
@@ -104,19 +201,21 @@ async def main():
         write_credentials(api_id, api_hash, phone_number)
 
     forwarder = TelegramForwarder(api_id, api_hash, phone_number)
-    choice = "1"
-    source_chat_id = -1002086003521  # Sostituisci con l'ID corretto o l'username del gruppo
-    destination_chat_id = -1002156987879  # Sostituisci con l'ID corretto o l'username del gruppo
-    sniper_chat_id = 6661880210  # sniper tg bot
-    quote_amount = 0.0046
+    choice = "2"
 
     if choice == "1":
         await forwarder.list_chats()
+
     elif choice == "2":
-        phrase = "Open time:"
-        await forwarder.forward_messages_to_channel(source_chat_id, destination_chat_id, phrase, sniper_chat_id, quote_amount)
+        bonkbot_caller_chat_id = -1002156987879  # Sostituisci con l'ID corretto o l'username del gruppo
+
+        await forwarder.handle_genesis_gem_message(bonkbot_caller_chat_id)
+
+        # await forwarder.handle_bonkbot_message(bonkbot_caller_chat_id)
+
     else:
         print("Invalid choice")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
